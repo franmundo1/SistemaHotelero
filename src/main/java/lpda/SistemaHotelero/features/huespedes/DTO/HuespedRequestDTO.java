82c2da806cf04ef119b9a155f0dd4cb37a886002
Cinthia Jesabel Ortiz <cinthiajesabel@gmail.com>
2026-05-05 15:00:23 -0300

    feat: creacion de los DTO
    Creacion de los DTO

---
+++ src/main/java/lpda/SistemaHotelero/features/huespedes/DTO/HuespedRequestDTO.java
@@ -0,0 +1,30 @@
+package lpda.SistemaHotelero.features.huespedes.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HuespedRequestDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    private String apellido;
+
+    @NotBlank(message = "El DNI es obligatorio")
+    private  String dni;
+
+    @Email(message = "El email no es valido")
+
+    private String email;
+
+    private  String telefono;
+}
