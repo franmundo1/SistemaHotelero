30970b33b7f16e7a7463f8f3c3025b94c5c93dfc
mauro_mdqmdp <mau_mdqmdp@live.com.ar>
2026-05-06 05:07:34 -0300

    feat: avance modulo canales reservas

---
+++ src/main/java/lpda/SistemaHotelero/features/canalesReservas/CanalReservaResponseDTO.java
@@ -0,0 +1,16 @@
+package lpda.SistemaHotelero.features.canalesReservas;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CanalReservaResponseDTO {
+
+    private Long idCanal;
+    private String nombrePersona;
+}
